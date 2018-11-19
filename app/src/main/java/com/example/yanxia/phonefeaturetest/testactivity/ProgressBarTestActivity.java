08fbb26a5eecaa74b56ceb00729a5bf041ab98a3
yanxia <yan.xia@ihandysoft.com>
2018-11-19 14:48:29 +0800

    add CircleProgressBar

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/ProgressBarTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/ProgressBarTestActivity.java
@@ -2,14 +2,39 @@ package com.example.yanxia.phonefeaturetest.testactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import com.example.yanxia.phonefeaturetest.R;
+import com.example.yanxia.phonefeaturetest.widget.CircleProgressBar;
 
+/**
+ * @author yanxia-Mac
+ */
 public class ProgressBarTestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_bar_test);
+
+        final CircleProgressBar circleProgressBar = findViewById(R.id.custom_progress_bar);
+
+        SeekBar seekBar = findViewById(R.id.test_seek_bar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                circleProgressBar.setProgress(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
