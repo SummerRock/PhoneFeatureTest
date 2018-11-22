67baa16ea78744e51ecb5f32a5713a2c6c84b98f
yanxia <yan.xia@ihandysoft.com>
2018-11-22 15:36:10 +0800

    add SnapHelper

--- app/src/main/java/com/example/yanxia/phonefeaturetest/horizonRv/HorizonRvTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/horizonRv/HorizonRvTestActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -23,7 +25,12 @@ public class HorizonRvTestActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.horizontal_rv);
         recyclerView.setAdapter(new HorizonRvTestAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false);
+
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
