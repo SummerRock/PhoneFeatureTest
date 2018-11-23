97f5a6d4ed35d678035da79649015453df57f5e0
yanxia <yan.xia@ihandysoft.com>
2018-11-23 11:02:06 +0800

    add test

---
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/StorageTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.yanxia.phonefeaturetest.testactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yanxia.phonefeaturetest.R;
+
+public class StorageTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_test);
+    }
+}
