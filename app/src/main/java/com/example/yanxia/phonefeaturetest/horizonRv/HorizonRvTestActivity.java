373e8c3d4c1cdde952a2f5db28a9295097b6f857
yanxia <yan.xia@ihandysoft.com>
2018-11-29 10:33:43 +0800

    add AdapterDataObserver

--- app/src/main/java/com/example/yanxia/phonefeaturetest/horizonRv/HorizonRvTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/horizonRv/HorizonRvTestActivity.java
@@ -2,13 +2,11 @@ package com.example.yanxia.phonefeaturetest.horizonRv;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SnapHelper;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -27,12 +25,44 @@ public class HorizonRvTestActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = findViewById(R.id.horizontal_rv);
-        recyclerView.setAdapter(new HorizonRvTestAdapter());
+        final HorizonRvTestAdapter adapter = new HorizonRvTestAdapter();
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+            }
+
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount) {
+                super.onItemRangeChanged(positionStart, itemCount);
+            }
+
+            @Override
+            public void onItemRangeChanged(int positionStart, int itemCount, @Nullable Object payload) {
+                super.onItemRangeChanged(positionStart, itemCount, payload);
+            }
+
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+            }
+
+            @Override
+            public void onItemRangeRemoved(int positionStart, int itemCount) {
+                super.onItemRangeRemoved(positionStart, itemCount);
+            }
+
+            @Override
+            public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+                super.onItemRangeMoved(fromPosition, toPosition, itemCount);
+            }
+        });
+        recyclerView.setAdapter(adapter);
         final LinearLayoutManager linearLayoutManager =
                 new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false);
 
-        SnapHelper snapHelper = new PagerSnapHelper();
-        snapHelper.attachToRecyclerView(recyclerView);
+        // SnapHelper snapHelper = new PagerSnapHelper();
+        // snapHelper.attachToRecyclerView(recyclerView);
 
         recyclerView.setLayoutManager(linearLayoutManager);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -50,8 +80,7 @@ public class HorizonRvTestActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                adapter.notifyItemRemoved(2);
             }
         });
     }
