f3a046abe044e612a1465c0b48f3d9cdfa7524cc
yanxia <yan.xia@ihandysoft.com>
2018-11-29 16:42:44 +0800

    add test code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/Myapplication.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/Myapplication.java
@@ -1,8 +1,11 @@
 package com.example.yanxia.phonefeaturetest;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
 
 /**
  * @author yanxia
@@ -12,11 +15,50 @@ import android.content.Context;
 public class Myapplication extends Application {
     @SuppressLint("StaticFieldLeak")
     private static Context context;
+    private static final String TAG = "Myapplication";
+
+    private ActivityLifecycleCallbacks activityLifecycleCallbacks = new ActivityLifecycleCallbacks() {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            Log.d(TAG, "onActivityCreated");
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+            Log.d(TAG, "onActivityStarted");
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            Log.d(TAG, "onActivityResumed");
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+            Log.d(TAG, "onActivityPaused");
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+            Log.d(TAG, "onActivityStopped");
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            Log.d(TAG, "onActivitySaveInstanceState");
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            Log.d(TAG, "onActivityDestroyed");
+        }
+    };
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        registerActivityLifecycleCallbacks(activityLifecycleCallbacks);
     }
 
     public static Context getContext() {
