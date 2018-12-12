59cf88bcadc799385dd7686c758b2c295567b185
yanxia <yan.xia@ihandysoft.com>
2018-12-12 15:15:16 +0800

    fix code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/TestString.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/TestString.java
@@ -1,5 +1,7 @@
 package com.example.yanxia.phonefeaturetest;
 
+import android.widget.Toast;
+
 import com.example.yanxia.phonefeaturetest.utils.CommonLog;
 
 /**
@@ -8,7 +10,8 @@ import com.example.yanxia.phonefeaturetest.utils.CommonLog;
 public class TestString {
 
     public static void testStringFormat() {
-        String str = String.format(Myapplication.getContext().getString(R.string.test_string_format), "test");
+        String str = String.format(Myapplication.getContext().getString(R.string.test_string_format), Myapplication.getContext().getString(R.string.app_name));
+        Toast.makeText(Myapplication.getContext(), str, Toast.LENGTH_SHORT).show();
         CommonLog.d(str);
     }
 }
