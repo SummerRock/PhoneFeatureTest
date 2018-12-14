72faf462a393fb42e09bae347b4b681fb7e9e176
yanxia <yan.xia@ihandysoft.com>
2018-12-14 15:12:17 +0800

    add test code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/AnimationTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/AnimationTestActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
@@ -27,20 +28,31 @@ public class AnimationTestActivity extends AppCompatActivity implements View.OnC
     }
 
     public void startAnimationType1(View view) {
+        imageView.clearAnimation();
         TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0,
                 Animation.RELATIVE_TO_SELF, 1, Animation.RELATIVE_TO_SELF, 0);
         animation.setFillAfter(true);
-        animation.setDuration(360);
+        animation.setDuration(5000);
         animation.setInterpolator(new FastOutSlowInInterpolator());
         imageView.startAnimation(animation);
     }
 
     public void startAnimationType2(View view) {
+        imageView.clearAnimation();
         TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0,
-                Animation.RELATIVE_TO_PARENT, 1, Animation.RELATIVE_TO_PARENT, 0);
+                Animation.RELATIVE_TO_PARENT, 0.8f, Animation.RELATIVE_TO_PARENT, 0.5f);
         animation.setFillAfter(true);
-        animation.setDuration(360);
-        animation.setInterpolator(new FastOutSlowInInterpolator());
+        animation.setDuration(5000);
+        animation.setInterpolator(new LinearInterpolator());
+        imageView.startAnimation(animation);
+    }
+
+    public void startAnimationType3(View view) {
+        imageView.clearAnimation();
+        TranslateAnimation animation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0,
+                Animation.ABSOLUTE, 200, Animation.ABSOLUTE, 50);
+        animation.setFillAfter(true);
+        animation.setDuration(5000);
         imageView.startAnimation(animation);
     }
 }
