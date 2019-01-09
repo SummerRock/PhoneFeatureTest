ca832025b63fe92df9c7b2e38025aaf99855f408
yanxia <yan.xia@ihandysoft.com>
2019-01-09 11:04:28 +0800

    add boring code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/multiProcess/MyContentProvider.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/multiProcess/MyContentProvider.java
@@ -9,14 +9,19 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.Random;
+
 public class MyContentProvider extends ContentProvider {
+    private static final String TAG = "MyContentProvider";
+
     public MyContentProvider() {
+        Log.i(TAG, "MyContentProvider init!");
     }
 
     @Nullable
     @Override
     public Bundle call(@NonNull String method, @Nullable String arg, @Nullable Bundle extras) {
-        Log.i("MyContentProvider", "call method: " + method);
+        Log.i(TAG, "call method: " + method);
         Bundle bundle = new Bundle();
         // put一个boolean值，如果执行方法成功返回true、失败返回false
         try {
@@ -24,7 +29,8 @@ public class MyContentProvider extends ContentProvider {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        bundle.putBoolean(method, true);
+        Random random = new Random();
+        bundle.putBoolean(method, random.nextBoolean());
         return bundle;
     }
 
