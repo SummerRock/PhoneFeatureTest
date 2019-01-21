268b19da483d031c61d9dea1b1f77f13175e89df
yanxia <yan.xia@ihandysoft.com>
2019-01-21 11:37:35 +0800

    add UI

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/FullscreenActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/FullscreenActivity.java
@@ -9,9 +9,12 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 import com.example.yanxia.phonefeaturetest.R;
+import com.example.yanxia.phonefeaturetest.utils.DisplayUtils;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -20,6 +23,8 @@ import com.example.yanxia.phonefeaturetest.R;
 public class FullscreenActivity extends AppCompatActivity {
 
     private static final String GIF_URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545740863490&di=e9db5a7b321cd18615fe15ba166c24e0&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20161113%2Fbec7f33085a9449182d2b92c2160288c_th.gif";
+
+    private static final String PIC_URL = "http://a.hiphotos.baidu.com/image/pic/item/aa18972bd40735faee21b63393510fb30e240862.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,8 @@ public class FullscreenActivity extends AppCompatActivity {
                 return false;
             }
         }).into(imageView);
+        ImageView imageView1 = findViewById(R.id.glide_test_image_view_1);
+        Glide.with(this).load(PIC_URL).apply(RequestOptions.bitmapTransform(new RoundedCorners(DisplayUtils.dpToPx(10)))).into(imageView1);
     }
 
 }
