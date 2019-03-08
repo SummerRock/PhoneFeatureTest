35f9599110bbc4f56592c545628a6c84a73620ad
yanxia <yan.xia@ihandysoft.com>
2019-03-08 16:35:07 +0800

    add auto size test view code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/ViewFeatureActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/ViewFeatureActivity.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.yanxia.phonefeaturetest.R;
 
 public class ViewFeatureActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private SeekBar seekBar;
+    private TextView autoSizeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +21,36 @@ public class ViewFeatureActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView_01);
         imageView.post(() -> imageView.setPivotX(imageView.getWidth() / 2f));
         imageView.setPivotY(1);
-        seekBar = findViewById(R.id.seekBar_01);
+        SeekBar seekBar = findViewById(R.id.seekBar_01);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 float f = 1 + progress / 100f;
                 imageView.setScaleX(f);
                 imageView.setScaleY(f);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        autoSizeTextView = findViewById(R.id.auto_size_test_tv);
+        //autoSizeTextView的 layout_width 不能是 wrap_content
+        SeekBar seekBar2 = findViewById(R.id.seekBar_02);
+        int baseHeight = autoSizeTextView.getLayoutParams().height;
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float progressFloat = progress / 50f;
+                autoSizeTextView.getLayoutParams().height = (int) (baseHeight + baseHeight * progressFloat);
+                autoSizeTextView.requestLayout();
             }
 
             @Override
