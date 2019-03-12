c3570b352dcad587e515419d844cb2d6b95dc30a
yanxia <yan.xia@ihandysoft.com>
2019-03-12 11:12:17 +0800

    add generic test code 2

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testjava/generics/GenericsTest.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testjava/generics/GenericsTest.java
@@ -61,10 +61,39 @@ public class GenericsTest {
         System.out.println("泛型测试 key value is " + obj.getKey());
     }
 
+    /**
+     * 泛型方法的基本介绍
+     *
+     * @param tClass 传入的泛型实参
+     * @return T 返回值为T类型
+     * 说明：
+     * 1）public 与 返回值中间<T>非常重要，可以理解为声明此方法为泛型方法。
+     * 2）只有声明了<T>的方法才是泛型方法，泛型类中的使用了泛型的成员方法并不是泛型方法。
+     * 3）<T>表明该方法将使用泛型类型T，此时才可以在方法中使用泛型类型T。
+     * 4）与泛型类的定义一样，此处T可以随便写为任意标识，常见的如T、E、K、V等形式的参数常用于表示泛型。
+     */
+    public <T> T genericMethod(Class<T> tClass) throws InstantiationException, IllegalAccessException {
+        T instance = tClass.newInstance();
+        return instance;
+    }
+
+    public void test7() {
+        try {
+            Object obj = genericMethod(Class.forName("java.util.ArrayList"));
+            System.out.println("泛型测试 class name is " + obj.getClass().getSimpleName());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]) {
         GenericsTest genericsTest = new GenericsTest();
         // genericsTest.showClassCastException();
         // genericsTest.test2();
-        genericsTest.test5();
+        genericsTest.test7();
     }
 }
