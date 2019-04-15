e020992ad8b428bf76d4f1ad086f26030f5716e2
yanxia <yan.xia@ihandysoft.com>
2019-04-15 15:32:51 +0800

    add layerType!

---
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/widget/DividerView.java
@@ -0,0 +1,64 @@
+package com.example.yanxia.phonefeaturetest.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.yanxia.phonefeaturetest.R;
+
+public class DividerView extends View {
+
+    static public int ORIENTATION_HORIZONTAL = 0;
+    static public int ORIENTATION_VERTICAL = 1;
+    private Paint mPaint;
+    private int orientation;
+
+    public DividerView(Context context) {
+        this(context, null);
+    }
+
+    public DividerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DividerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        int dashGap, dashLength, dashThickness;
+        int color;
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DividerView, 0, 0);
+
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        try {
+            dashGap = a.getDimensionPixelSize(R.styleable.DividerView_dashGap, 5);
+            dashLength = a.getDimensionPixelSize(R.styleable.DividerView_dashLength, 5);
+            dashThickness = a.getDimensionPixelSize(R.styleable.DividerView_dashThickness, 3);
+            color = a.getColor(R.styleable.DividerView_color, 0xff000000);
+            orientation = a.getInt(R.styleable.DividerView_orientation, ORIENTATION_HORIZONTAL);
+        } finally {
+            a.recycle();
+        }
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(color);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(dashThickness);
+        mPaint.setPathEffect(new DashPathEffect(new float[]{dashLength, dashGap,}, 0));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (orientation == ORIENTATION_HORIZONTAL) {
+            float center = getHeight() * .5f;
+            canvas.drawLine(0, center, getWidth(), center, mPaint);
+        } else {
+            float center = getWidth() * .5f;
+            canvas.drawLine(center, 0, center, getHeight(), mPaint);
+        }
+    }
+}
