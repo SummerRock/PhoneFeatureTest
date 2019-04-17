c40bc83ec5d2654e24928e7309e018bf34daaacb
yanxia <yan.xia@ihandysoft.com>
2019-04-17 11:13:10 +0800

    add scratch

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/TouchTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/TouchTestActivity.java
@@ -2,17 +2,39 @@ package com.example.yanxia.phonefeaturetest.testactivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.yanxia.phonefeaturetest.R;
+import com.example.yanxia.phonefeaturetest.widget.ScratchView;
 
 /**
  * @author yanxia-Mac
  */
 public class TouchTestActivity extends AppCompatActivity {
 
+    private static final String TAG = "ScratchView";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch_test);
+
+        ScratchView scratchView = findViewById(R.id.scratch_view);
+        scratchView.setEraseStatusListener(new ScratchView.EraseStatusListener() {
+            @Override
+            public void onProgress(int percent) {
+                Log.i(TAG, "onProgress mark: " + percent);
+            }
+
+            @Override
+            public void onCompleted(View view) {
+                Log.i(TAG, "onCompleted mark: ");
+                Toast.makeText(TouchTestActivity.this, "完成了！", Toast.LENGTH_SHORT).show();
+                scratchView.clear();
+            }
+        });
+
     }
 }
