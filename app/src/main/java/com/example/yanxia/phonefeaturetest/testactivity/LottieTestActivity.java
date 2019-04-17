b82cdbe9c7d8569c9ab09d762ff5d9e3ec47bda0
yanxia <yan.xia@ihandysoft.com>
2019-04-17 14:20:05 +0800

    add

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/LottieTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/LottieTestActivity.java
@@ -1,8 +1,10 @@
 package com.example.yanxia.phonefeaturetest.testactivity;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.yanxia.phonefeaturetest.R;
 
 public class LottieTestActivity extends AppCompatActivity {
@@ -11,5 +13,9 @@ public class LottieTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lottie_test);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.test_lottie_1);
+        lottieAnimationView.updateBitmap("image_3", BitmapFactory.decodeResource(getResources(), R.drawable.test_image_01));
+        lottieAnimationView.playAnimation();
     }
 }
