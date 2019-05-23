34907233f1be402f0188a09d6f7d9c0531db117a
yanxia <yan.xia@ihandysoft.com>
2019-05-23 17:40:01 +0800

    add bitmap code

--- app/src/main/java/com/example/yanxia/phonefeaturetest/utils/DisplayUtils.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/utils/DisplayUtils.java
@@ -2,6 +2,13 @@ package com.example.yanxia.phonefeaturetest.utils;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
@@ -30,12 +37,30 @@ public class DisplayUtils {
             resourceName = resourceName.substring(0, dotPosition);
         }
 
-        try {
-            return context.getResources().getIdentifier(resourceName, type, context.getPackageName());
-        } catch (Exception e) {
-            e.printStackTrace();
+        int resID = context.getResources().getIdentifier(resourceName, type, context.getPackageName());
+        if (resID == 0) {
+            Log.e("common", "getResourceIdByName wrong! resourceName: " + resourceName);
         }
-        Log.e("common", "getResourceIdByName wrong! resourceName: " + resourceName);
         return 0;
+    }
+
+    public static Bitmap getRoundCornerBitmap(@NonNull Bitmap bitmap, int cornerRadius) {
+        float roundPx = dpToPx(cornerRadius);
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
