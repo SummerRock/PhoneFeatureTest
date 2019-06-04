ae5bc4419c754216dca8b91a687548f57de1526f
yanxia <yan.xia@ihandysoft.com>
2019-06-04 14:34:00 +0800

    update

--- app/src/main/java/com/example/yanxia/phonefeaturetest/download/test/DownloadTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/download/test/DownloadTestActivity.java
@@ -18,6 +18,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import okhttp3.Call;
@@ -41,6 +43,19 @@ public class DownloadTestActivity extends AppCompatActivity implements OnDownloa
     private ProgressBar progressBar;
     private TextView progressTextView;
     private DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+    private List<DownloadDemoItem> getDemoList() {
+        List<DownloadDemoItem> downloadDemoItemList = new ArrayList<>();
+        downloadDemoItemList.add(new DownloadDemoItem("tokyo", "demo", "http://speedtest.tokyo.linode.com/100MB-tokyo.bin", LOCALE_FILE_DIR_NAME, "demo_file_tokyo.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("london", "demo", "http://speedtest.london.linode.com/100MB-london.bin", LOCALE_FILE_DIR_NAME, "demo_file_london.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("newark", "demo", "http://speedtest.newark.linode.com/100MB-newark.bin", LOCALE_FILE_DIR_NAME, "demo_file_newark.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("atlanta", "demo", "http://speedtest.atlanta.linode.com/100MB-atlanta.bin", LOCALE_FILE_DIR_NAME, "demo_file_atlanta.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("dallas", "demo", "http://speedtest.dallas.linode.com/100MB-dallas.bin", LOCALE_FILE_DIR_NAME, "demo_file_dallas.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("fremont", "demo", "http://speedtest.fremont.linode.com/100MB-fremont.bin", LOCALE_FILE_DIR_NAME, "demo_file_fremont.bin"));
+        downloadDemoItemList.add(new DownloadDemoItem("cachefly", "demo", "http://cachefly.cachefly.net/100mb.test", LOCALE_FILE_DIR_NAME, "demo_file_cachefly.test"));
+        downloadDemoItemList.add(new DownloadDemoItem("direct", "demo", "http://direct.netcloud.com/100MB.BIN", LOCALE_FILE_DIR_NAME, "demo_file_direct.bin"));
+        return downloadDemoItemList;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
