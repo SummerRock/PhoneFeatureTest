b96de6d23f6e53b5b1470d88515052fd802e8f13
yanxia <yan.xia@ihandysoft.com>
2019-06-05 17:41:14 +0800

    update

--- app/src/main/java/com/example/yanxia/phonefeaturetest/recyclerview/CustomRecyclerViewActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/recyclerview/CustomRecyclerViewActivity.java
@@ -3,6 +3,7 @@ package com.example.yanxia.phonefeaturetest.recyclerview;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.yanxia.phonefeaturetest.R;
 import com.example.yanxia.phonefeaturetest.recyclerview.data.Demo;
@@ -13,20 +14,30 @@ import java.util.List;
 
 public class CustomRecyclerViewActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_recycler_view);
+
+        recyclerView = findViewById(R.id.recycler_view_demo);
     }
 
     private List<DemoGroup> getData() {
         List<DemoGroup> demoGroupList = new ArrayList<>();
-        DemoGroup demoGroupA = new DemoGroup(getDemoList(0, 1, "demoGroupA"), "demoGroupA");
-        DemoGroup demoGroupB = new DemoGroup(getDemoList(1, 2, "demoGroupB"), "demoGroupB");
-        DemoGroup demoGroupC = new DemoGroup(getDemoList(2, 5, "demoGroupC"), "demoGroupC");
-        demoGroupList.add(demoGroupA);
-        demoGroupList.add(demoGroupB);
-        demoGroupList.add(demoGroupC);
+        DemoGroup parentA = new DemoGroup(getDemoList(0, 1, "parentA"), "parentA");
+        DemoGroup parentB = new DemoGroup(getDemoList(1, 2, "parentB"), "parentB");
+        DemoGroup parentC = new DemoGroup(getDemoList(2, 5, "parentC"), "parentC");
+        DemoGroup parentD = new DemoGroup(getDemoList(6, 10, "parentD"), "parentD");
+        DemoGroup parentE = new DemoGroup(getDemoList(11, 13, "parentE"), "parentE");
+        DemoGroup parentF = new DemoGroup(getDemoList(13, 14, "parentF"), "parentF");
+        demoGroupList.add(parentA);
+        demoGroupList.add(parentB);
+        demoGroupList.add(parentC);
+        demoGroupList.add(parentD);
+        demoGroupList.add(parentE);
+        demoGroupList.add(parentF);
         return demoGroupList;
     }
 
