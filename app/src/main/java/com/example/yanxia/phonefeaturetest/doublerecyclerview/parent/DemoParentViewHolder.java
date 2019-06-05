6304878a5d331f85e15335f2df89afd9036664a3
yanxia <yan.xia@ihandysoft.com>
2019-06-05 20:50:10 +0800

    update

--- app/src/main/java/com/example/yanxia/phonefeaturetest/doublerecyclerview/parent/DemoParentViewHolder.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/doublerecyclerview/parent/DemoParentViewHolder.java
@@ -1,18 +1,23 @@
 package com.example.yanxia.phonefeaturetest.doublerecyclerview.parent;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.yanxia.phonefeaturetest.R;
+
 public class DemoParentViewHolder extends RecyclerView.ViewHolder {
 
+    public TextView name;
     public RecyclerView recyclerView;
 
     public DemoParentViewHolder(@NonNull View itemView) {
         super(itemView);
-        recyclerView = (RecyclerView) itemView;
+        name = itemView.findViewById(R.id.parent_item_name);
+        recyclerView = itemView.findViewById(R.id.parent_item_rv);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(manager);
     }
