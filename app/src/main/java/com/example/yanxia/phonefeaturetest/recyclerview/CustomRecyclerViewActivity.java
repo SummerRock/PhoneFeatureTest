ac24d82506ca0575cdc8ff9a9d3375f7e9edd962
yanxia <yan.xia@ihandysoft.com>
2019-06-05 16:40:50 +0800

    update

---
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/recyclerview/CustomRecyclerViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.yanxia.phonefeaturetest.recyclerview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yanxia.phonefeaturetest.R;
+
+public class CustomRecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_recycler_view);
+    }
+}
