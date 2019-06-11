6d6096bd2e28a39789e27ebc7d1b9ba7057d8e30
yanxia <yan.xia@ihandysoft.com>
2019-06-11 20:51:57 +0800

    update

--- app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/GpsTestActivity.java
+++ app/src/main/java/com/example/yanxia/phonefeaturetest/testactivity/GpsTestActivity.java
@@ -1,12 +1,20 @@
 package com.example.yanxia.phonefeaturetest.testactivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.caverock.androidsvg.RenderOptions;
+import com.caverock.androidsvg.SVG;
 import com.caverock.androidsvg.SVGImageView;
+import com.caverock.androidsvg.SVGParseException;
 import com.example.yanxia.phonefeaturetest.R;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 
 public class GpsTestActivity extends AppCompatActivity {
@@ -16,6 +24,10 @@ public class GpsTestActivity extends AppCompatActivity {
     private static final String COLOR_DISABLE = "#641D1D";
 
     private SVGImageView puzzleImageView;
+
+    private RenderOptions renderOptions = new RenderOptions();
+
+    private SVG localeSvg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +41,22 @@ public class GpsTestActivity extends AppCompatActivity {
         dataArray[5] = 2;
         dataArray[8] = 2;
         String css = getCssStringFromDataArray(dataArray);
-        puzzleImageView.setCSS(css);
+        renderOptions.css(css);
+
+        initLocaleSvg();
+    }
+
+    public void updateSvg(View view) {
+        localeSvg.renderToPicture(renderOptions);
+        puzzleImageView.setSVG(localeSvg);
+
+        // long[] dataArray = new long[9];
+        // dataArray[3] = 2;
+        // dataArray[5] = 2;
+        // dataArray[8] = 2;
+        // String css = getCssStringFromDataArray(dataArray);
+        //
+        // puzzleImageView.setCSS(css);
     }
 
     private String getCssStringFromDataArray(long[] dataArray) {
@@ -44,5 +71,55 @@ public class GpsTestActivity extends AppCompatActivity {
             }
         }
         return cssString.toString();
+    }
+
+    private void initLocaleSvg() {
+        // Check whether svg attribute is a string.
+        // Could be a URL/filename or an SVG itself
+        String url = "puzzle_nine.svg";
+        if (internalSetImageAsset(url))
+            return;
+        // Last chance, maybe there is an actual SVG in the string
+        // If the SVG is in the string, then we will assume it is not very large, and thus doesn't need to be parsed in the background.
+        setFromString(url);
+    }
+
+    private boolean internalSetImageAsset(String filename) {
+        try {
+            InputStream is = getAssets().open(filename);
+            new LoadURITask().execute(is);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+
+    }
+
+    private class LoadURITask extends AsyncTask<InputStream, Integer, SVG> {
+        protected SVG doInBackground(InputStream... is) {
+            try {
+                return SVG.getFromInputStream(is[0]);
+            } catch (SVGParseException e) {
+                Log.e("SVGImageView", "Parse error loading URI: " + e.getMessage());
+            } finally {
+                try {
+                    is[0].close();
+                } catch (IOException e) { /* do nothing */ }
+            }
+            return null;
+        }
+
+        protected void onPostExecute(SVG svg) {
+            localeSvg = svg;
+        }
+    }
+
+    private void setFromString(String url) {
+        try {
+            localeSvg = SVG.getFromString(url);
+        } catch (SVGParseException e) {
+            // Failed to interpret url as a resource, a filename, or an actual SVG...
+            Log.e("SVGImageView", "Could not find SVG at: " + url);
+        }
     }
 }
