1a321ff1709b7032b67332a8b37857133718c737
yanxia <yan.xia@ihandysoft.com>
2019-06-14 20:00:05 +0800

    update

---
+++ pluginlibrary/src/main/java/com/example/pluginlibrary/PluginActivity.java
@@ -0,0 +1,38 @@
+package com.example.pluginlibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PluginActivity extends AppCompatActivity implements IPlugin {
+
+    private int mFrom = FROM_INTERNAL;
+    private Activity mProxyActivity;
+
+    @Override
+    public void attach(Activity proxyActivity) {
+        this.mProxyActivity = proxyActivity;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mFrom = savedInstanceState.getInt("FROM");
+        }
+
+        if (mFrom == FROM_INTERNAL) {
+            super.onCreate(savedInstanceState);
+            mProxyActivity = this;
+        }
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        if (mFrom == FROM_INTERNAL) {
+            super.setContentView(layoutResID);
+        } else {
+            mProxyActivity.setContentView(layoutResID);
+        }
+    }
+}
