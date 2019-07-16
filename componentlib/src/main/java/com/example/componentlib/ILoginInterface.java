5b4287b67130a0b6922736e00b2c432c5ab48ee2
yanxia <yan.xia@ihandysoft.com>
2019-07-16 15:10:26 +0800

    update

---
+++ componentlib/src/main/java/com/example/componentlib/ILoginInterface.java
@@ -0,0 +1,10 @@
+package com.example.componentlib;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public interface ILoginInterface {
+
+    void launch(@NonNull Context context);
+}
